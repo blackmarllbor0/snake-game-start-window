d17ca202b37cd9d7d6affa1af504fa47691525aa
blacmarllbor0 <3100194@gmail.com>
2023-01-12 14:16:52 +0300

    сделал список из пяти лучших игроков

--- display/record.go
+++ display/record.go
@@ -1,6 +1,10 @@
 package display
 
 import (
+	"fmt"
+	"snake/players"
+	"sort"
+
 	"github.com/gdamore/tcell/v2"
 	tview "github.com/rivo/tview"
 )
@@ -13,9 +17,15 @@ type RecordDisplay struct {
 
 // init инициализирует блок
 func (r *RecordDisplay) init() *tview.Flex {
+	var flex = tview.NewFlex().
+		AddItem(tview.NewBox(), 0, 1, false).
+		AddItem(r.createList(), 0, 4, false).
+		AddItem(tview.NewBox(), 0, 1, false)
+
 	r.block = tview.NewFlex().
 		SetDirection(tview.FlexRow).
-		AddItem(r.createLabel(), 0, 1, false)
+		AddItem(r.createLabel(), 0, 1, false).
+		AddItem(flex, 0, 3, false)
 	return r.block
 }
 
@@ -30,4 +40,27 @@ func (r *RecordDisplay) createLabel() *tview.TextView {
 	r.label.SetTextColor(tview.AlignCenter)
 	r.label.SetBackgroundColor(cb)
 	return r.label
+}
+
+func (r *RecordDisplay) createList() *tview.List {
+	r.bestPlayers = tview.NewList()
+	r.bestPlayers.SetTitleAlign(tview.AlignCenter)
+
+	var playerList = players.GetPlayers()
+
+	sort.Slice(playerList, func(i, j int) bool {
+		return playerList[i].GetBestScore() > playerList[j].GetBestScore()
+	})
+
+	for i := 0; i < 6; i++ {
+		var currentPlayer = playerList[i]
+		if currentPlayer.GetBestScore() > 0 {
+			var player = fmt.Sprintf(
+				"%s - %d\n", currentPlayer.GetName(), currentPlayer.GetBestScore(),
+			)
+			r.bestPlayers.AddItem(player, "", tcell.RuneVLine, nil)
+		}
+	}
+
+	return r.bestPlayers
 }
