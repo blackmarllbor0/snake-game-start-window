20e1165d431c424f21f09ecfa6fdeec8ec229536
blacmarllbor0 <3100194@gmail.com>
2023-01-21 17:47:26 +0300

    почистил то, что делать не буду

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"snake/display"
 	"snake/players"
 )
@@ -10,13 +8,5 @@ import (
 func main() {
 	players.CheckDir()
 	display := display.Display{}
-
-	// будет выбора запуска игры через два вида интерфейса
-	if len(os.Args) <= 1 {
-		// для запуска игры через граф. интерфейс
-		display.Displaying()
-	} else {
-		// для запуск игры через передачу аргументов
-		fmt.Println(os.Args[1:])
-	}
+	display.Displaying()
 }
